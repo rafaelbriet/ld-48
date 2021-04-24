c063ac3213696173827111b4715bfc9d75db6db1
Rafael Briet <rafael-briet@hotmail.com>
2021-04-24 10:46:34 -0300

    Add traps and player death

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public event Action CharacterDied;
+
+    private void OnDisable()
+    {
+        CharacterDied?.Invoke();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerPrefab;
+
+    private GameObject player;
+
+    private void Awake()
+    {
+        Transform playerSpawnPoint = GameObject.FindGameObjectWithTag("PlayerSpawnPoint").transform;
+
+        player = Instantiate(playerPrefab, playerSpawnPoint.position, Quaternion.identity);
+
+        player.GetComponent<Character>().CharacterDied += OnPlayerDeath;
+    }
+
+    private void OnPlayerDeath()
+    {
+        RestartLevel();
+    }
+
+    private void RestartLevel()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
